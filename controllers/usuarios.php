58e07d84abb4c53e7b7108b8efe6821294eeaf21
CrHerrera97 <cristian.2221@hotmail.es>
2026-09-11 11:38:17 -0500

    creacion crud usuarios

--- controllers/usuarios.php
+++ controllers/usuarios.php
@@ -5,6 +5,28 @@ declare(strict_types=1);
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../config/auth.php';
 
+$origen = $_SERVER['HTTP_ORIGIN'] ?? '';
+$origenesPermitidos = [
+    'http://localhost',
+    'http://127.0.0.1',
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
+    'null'
+];
+
+if (in_array($origen, $origenesPermitidos, true)) {
+    header("Access-Control-Allow-Origin: {$origen}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Vary: Origin');
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
+    http_response_code(204);
+    exit;
+}
+
 class UsuariosController
 {
     public function __construct(private PDO $pdo)
